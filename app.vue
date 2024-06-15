cb3d68009fc039fb1fd7ff86bc2c548e5e471510
Ryan Weston <ryanlweston@gmail.com>
2024-06-15 22:48:25 +0100

    feat: tracking

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      'src': 'https://plausible.io/js/script.js',
+      'defer': true,
+      'data-domain': 'rudimentary.dev',
+    },
+  ],
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
