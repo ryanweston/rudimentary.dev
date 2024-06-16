539acf786dbab620a383edcf3098f29ddb2fb2dc
Ryan Weston <ryanlweston@gmail.com>
2024-06-16 12:53:13 +0100

    chore: add `web-dev` category

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,4 +1,5 @@
 export const categoryDescriptions: { [key: string]: string } = {
   'concepts': 'Concepts are the building blocks of the language. They are the words that we use to describe the world around us.',
   'computer-science': 'Computer Science is the study of computers and computational systems. It is a broad field that encompasses many different areas.',
+  'web-dev': 'Web Development is the process of building and maintaining websites and web applications. It involves a variety of skills and technologies.',
 }
