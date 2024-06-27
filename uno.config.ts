9d419925bde9614377957ffc682bfbc2730284f0
Ryan Weston <ryanlweston@gmail.com>
2024-06-27 22:28:14 +0100

    fix: unocss preflight class compile

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, transformerDirectives } from 'unocss'
 
 export default defineConfig({
+  transformers: [
+    transformerDirectives({ enforce: 'pre' }),
+  ],
   theme: {
     fontFamily: {
-    //   sans: ['Inter var', 'Inter', 'sans-serif'],
       sans: ['IBM Plex Mono'],
       mono: ['IBM Plex Mono'],
     },
